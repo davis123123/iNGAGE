b6cca1c4543f0e5af6a6ed3ec12e7ec6f2f2eaaf
davis123123 <daviswong123@gmail.com>
2017-05-19 16:37:28 -0700

    Fix earlier push on leaveroom
    made commit on wrong file

--- app/src/main/java/ingage/ingage20/handlers/ChatRoomHandler.java
+++ app/src/main/java/ingage/ingage20/handlers/ChatRoomHandler.java
@@ -35,7 +35,7 @@ public class ChatRoomHandler  extends AsyncTask<String, String, String> {
         //TODO need change for server change
         String view_url ="http://24.7.128.143/view_chatroom_status.php";     //10.0.2.2 CHANGE FOR OTHER SERVER
         String join_url ="http://24.7.128.143/join_chatroom.php";
-
+        String leave_url = "http://10.0.0.199/leave_room.php";
 
         if (type.equals("view")) {
             try {
@@ -80,6 +80,45 @@ public class ChatRoomHandler  extends AsyncTask<String, String, String> {
                 String user = params[2];
                 String side = params[3];
                 URL url = new URL(join_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data =
+                        URLEncoder.encode("thread_id","UTF-8")+"="+ URLEncoder.encode(thread_id,"UTF-8")+"&"+
+                                URLEncoder.encode("user", "UTF-8")+"="+URLEncoder.encode(user,"UTF-8")+"&"+
+                                URLEncoder.encode("side","UTF-8")+"="+ URLEncoder.encode(side,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader =
+                        new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+                bufferedReader.close();;
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        else if (type.equals("leave")) {
+            try {
+                String thread_id = params[1];
+                String user = params[2];
+                String side = params[3];
+                URL url = new URL(leave_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
