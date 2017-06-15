5c2d36be2bf1f0796d78c7072f2ce491ca20a4b8
ViWu <viwu@ucdavis.edu>
2017-06-14 18:21:20 -0700

    Initialized upload avatar
    Created base for uploading images for avatar in user profile.

--- app/src/main/java/ingage/ingage20/activities/UserProfileActivity.java
+++ app/src/main/java/ingage/ingage20/activities/UserProfileActivity.java
@@ -1,9 +1,21 @@
 package ingage.ingage20.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
@@ -17,6 +29,10 @@ import ingage.ingage20.managers.SessionManager;
 public class UserProfileActivity extends AppCompatActivity {
 
     String username, email, tribute_pts, subs;
+    Button upload;
+    ImageView avatar;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +58,81 @@ public class UserProfileActivity extends AppCompatActivity {
         pts_info.setText(tribute_pts);
         subs_info.setText(subs);
 
+        upload = (Button) findViewById(R.id.upload_profile_img);
+        avatar = (ImageView) findViewById(R.id.prof_img_preview);
 
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goUploadImage();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (Build.VERSION.SDK_INT >= 23) {
+                    Log.d("STATE", "API LVL >= 23");
+                    if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                    } else {
+
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    }
+                }
+
+                else {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+
+
+                    } else {
+                        Toast.makeText(this, "Permission denied to access external storage!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return;
+            }
+        }
+    }
+
+    private void goUploadImage(){
+
+        Context mContext = getApplicationContext();
+        int check = mContext.getPackageManager().checkPermission(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                mContext.getPackageName());
+        if (check == PackageManager.PERMISSION_GRANTED) {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+        }
+
+        else
+            // Required to ask user for permission to access user's external storage
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String filename = getContentResolver().getType(selectedImage);
+            avatar.setImageURI(selectedImage);
+        }
     }
 
 
---
+++ app/src/main/java/ingage/ingage20/handlers/UploadAvatarHandler.java
@@ -0,0 +1,93 @@
+package ingage.ingage20.handlers;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Base64;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by wuv66 on 6/14/2017.
+ */
+
+public class UploadAvatarHandler extends AsyncTask<String, String, String>{
+
+    Bitmap image;
+    public UploadAvatarHandler(Bitmap image) {
+        this.image = image;
+
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        String post_image_url = "http://10.0.0.199/upload_avatar.php";
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+
+        try {
+            /**Map<String, String> dataToSend = new HashMap<>();
+             dataToSend.put("name", name);
+             dataToSend.put("image", encodedImage);**/
+            String user_name = params[0];
+
+            URL url = new URL(post_image_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String post_data =
+                    URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&" +
+                            URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(encodedImage, "UTF-8");
+
+
+            /**String data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(encodedImage.toString(), "UTF-8");
+             data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode(API_KEY, "UTF-8");
+             **/
+            bufferedWriter.write(post_data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader =
+                    new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+            String result = "";
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            ;
+            inputStream.close();
+            httpURLConnection.disconnect();
+            return result;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+    }
+}
