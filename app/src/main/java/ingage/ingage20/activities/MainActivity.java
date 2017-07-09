3353eb298cf0a19e101466ef39504bb457e5307c
ViWu <viwu@ucdavis.edu>
2017-07-09 16:25:58 -0700

    Implemented onResume() method for Main Activity
    Fixed case in which the user changes his/her avatar and presses the back button to go from UserProfileActivity to MainActivity. Now the avatar updates since the app downloads from the server when back is pressed, so that the user can see the change.

--- app/src/main/java/ingage/ingage20/activities/MainActivity.java
+++ app/src/main/java/ingage/ingage20/activities/MainActivity.java
@@ -343,6 +343,13 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onResume()
+    {
+        super.onResume();
+        downloadAvatar();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
