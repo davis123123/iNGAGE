47c980b1cbe317451f6b9d771e0eeeaa33c918bc
ViWu <viwu@ucdavis.edu>
2018-01-15 11:39:53 -0800

    Fixed emulator wifi issue
    Updated wifi manager to allow us developers to test on emulator. Debug builds can be tested on the emulator or physical devices, while release builds we'll be testing on physical devices. When testing on emulator, just make sure the wifi is on.

--- app/src/main/java/ingage/ingage20/managers/WifiManager.java
+++ app/src/main/java/ingage/ingage20/managers/WifiManager.java
@@ -3,6 +3,10 @@ package ingage.ingage20.managers;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.util.Log;
+
+import ingage.ingage20.BuildConfig;
 
 /**
  * Created by wuv66 on 12/18/2017.
@@ -20,15 +24,53 @@ public class WifiManager {
         ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        if (mWifi.isConnected()) {
-            return true;
-        }
-
-        else{
+        //check if running on emulator and using debug build
+        if(isEmulator() && BuildConfig.BUILD_TYPE.equals("debug")){
+            ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivity != null) {
+                NetworkInfo[] info = connectivity.getAllNetworkInfo();
+                if (info != null)
+                    for (int i = 0; i < info.length; i++) {
+                        Log.i("STATE", "get emulator state: " + info[i].getState());
+                        if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                            return true;
+                        }
+                    }
+            }
             return false;
         }
+
+        //running on device
+        else {
+            if (mWifi.isConnected()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
     }
 
 
+    public boolean isEmulator(){
+
+        boolean result= Build.FINGERPRINT.startsWith("generic")
+                        ||Build.FINGERPRINT.startsWith("unknown")
+                        ||Build.MODEL.contains("google_sdk")
+                        ||Build.MODEL.contains("Emulator")
+                        ||Build.MODEL.contains("Android SDK built for x86");
+        if(result) {
+            Log.i("STATE", "app is running on emulator");
+            return true;
+        }
+
+        result|=Build.BRAND.startsWith("generic")&&Build.DEVICE.startsWith("generic");
+        if(result) {
+            Log.i("STATE", "app is running on emulator");
+            return true;
+        }
+        //result|="google_sdk".equals(Build.PRODUCT);
+        return result;
+    }
 
 }
