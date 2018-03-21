65866a0321ef6d541ae1c8ec40dcfdf12b041c77
davis123123 <daviswong123@gmail.com>
2018-03-20 20:27:16 -0700

    Setup userInfo handler for userprofile
    makes another call to server to retrieve selected user's details

--- app/src/main/java/ingage/ingage20/fragments/UserInfoFragment.java
+++ app/src/main/java/ingage/ingage20/fragments/UserInfoFragment.java
@@ -1,20 +1,28 @@
 package ingage.ingage20.fragments;
 
-import android.content.Context;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import ingage.ingage20.R;
+import ingage.ingage20.activities.UserProfileActivity;
 import ingage.ingage20.adapters.UserProfileInfoAdapter;
+import ingage.ingage20.handlers.UserInfoHandler;
 import ingage.ingage20.managers.SessionManager;
 
 
@@ -22,49 +30,77 @@ public class UserInfoFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     RecyclerView recycler;
+    private View mLoadingView;
     UserProfileInfoAdapter adapter = new UserProfileInfoAdapter();
     String username, email, tribute_pts, subs, date_joined;
     protected static ArrayList<String> sub_arr = new ArrayList<>();
     String result = "Subscriptions: ";
-
-    public UserInfoFragment() {
-
-    }
-
-
-    public static UserInfoFragment newInstance() {
-        UserInfoFragment fragment = new UserInfoFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    View rootView;
+    private int mShortAnimationDuration;
+    UserProfileActivity userProfileActivity;
+    public static UserInfoHandler handler;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        userProfileActivity = (UserProfileActivity) getActivity();
 
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_user_info, container, false);
+        rootView = inflater.inflate(R.layout.fragment_user_info, container, false);
+        recycler = (RecyclerView) rootView.findViewById(R.id.info);
+        mLoadingView = rootView.findViewById(R.id.loading_spinner);
+        SessionManager session = new SessionManager(getContext());
+        HashMap<String, String> info = session.getUserDetails();
+        if(userProfileActivity.getUsername().equals(info.get(SessionManager.KEY_NAME))) {
+            mLoadingView.setVisibility(View.GONE);
+            final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+            recycler.setLayoutManager(layoutManager);
+            adapter = new UserProfileInfoAdapter();
+            recycler.setAdapter(adapter);
+
+            username = info.get(SessionManager.KEY_NAME);
+            email = info.get(SessionManager.KEY_EMAIL);
+            tribute_pts = info.get(SessionManager.KEY_TRIBUTE_POINTS);
+            subs = info.get(SessionManager.KEY_SUBSCRIPTIONS);
+            date_joined = info.get(SessionManager.KEY_DATE_JOINED);
+            if (email != null && email.length() > 0)
+                adapter.add("Email: " + email);
+            else
+                adapter.add("Email: N/A");
+            adapter.add("Tribute points: " + tribute_pts);
+            setSubscriptions();
+            adapter.add("Date joined: " + date_joined);
+            adapter.notifyDataSetChanged();
+        }
+        else{
+            UserInfoHandler.CallBackData callBackData = new UserInfoHandler.CallBackData() {
+                @Override
+                public void notifyChange(String serverResponse) {
+                    Log.d("SERVERREeeSPONSE", serverResponse);
+                    parseProfileJSON(serverResponse);
+                    crossFadeRecyler();
+                }
+            };
+            handler = new UserInfoHandler();
+            handler.setCallBackData(callBackData);
+            handler.enqueue(userProfileActivity.getUsername());
+            recycler.setVisibility(View.GONE);
+            mShortAnimationDuration = getResources().getInteger(
+                    android.R.integer.config_shortAnimTime);
+        }
+        return rootView;
+    }
+
+    private void crossFadeRecyler() {
         recycler = (RecyclerView) rootView.findViewById(R.id.info);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recycler.setLayoutManager(layoutManager);
         adapter = new UserProfileInfoAdapter();
         recycler.setAdapter(adapter);
-
-        SessionManager session = new SessionManager(getContext());
-        HashMap<String, String> info = session.getUserDetails();
-        username = info.get(SessionManager.KEY_NAME);
-        email = info.get(SessionManager.KEY_EMAIL);
-        tribute_pts = info.get(SessionManager.KEY_TRIBUTE_POINTS);
-        subs = info.get(SessionManager.KEY_SUBSCRIPTIONS);
-        date_joined = info.get(SessionManager.KEY_DATE_JOINED);
 
         if(email != null && email.length() > 0)
             adapter.add("Email: " + email);
@@ -74,11 +110,31 @@ public class UserInfoFragment extends Fragment {
         setSubscriptions();
         adapter.add("Date joined: " + date_joined);
         adapter.notifyDataSetChanged();
-        return rootView;
+
+        recycler.setAlpha(0f);
+        recycler.setVisibility(View.VISIBLE);
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        recycler.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        mLoadingView.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mLoadingView.setVisibility(View.GONE);
+                    }
+                });
     }
 
     //Parse the thread subscriptions JSON string
     protected ArrayList parseSubs(String thread_subscriptions){
+
         thread_subscriptions = thread_subscriptions.replace("["," ");
         thread_subscriptions = thread_subscriptions.replace("]"," ");
         sub_arr.clear();
@@ -92,6 +148,28 @@ public class UserInfoFragment extends Fragment {
         }
 
         return sub_arr;
+    }
+
+    protected void parseProfileJSON(String json_string){
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+        Log.i("STATE", "userjson string: " + json_string);
+
+        try {
+            jsonObject = new JSONObject(json_string);
+            jsonArray = jsonObject.getJSONArray("users");
+            int count= 0;
+            while(count < jsonArray.length()){
+                JSONObject JO = jsonArray.getJSONObject(count);
+                email = JO.getString("email");
+                tribute_pts = JO.getString("tribute_points");
+                subs = JO.getString("thread_subscriptions");
+                date_joined = JO.getString("date_joined");
+                count++;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void setSubscriptions(){
