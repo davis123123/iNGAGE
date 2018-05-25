dc56d00d3eeab29a3ccd6a95d8ca89fd86217e38
ViWu <viwu@ucdavis.edu>
2018-05-24 23:53:01 -0700

    Removed Picasso indicators for images
    The colored corners no longer show up on images. It should only be turned on for debug builds on the private/test server.

--- app/src/main/java/ingage/ingage/App.java
+++ app/src/main/java/ingage/ingage/App.java
@@ -23,7 +23,7 @@ public class App extends Application {
         Picasso.Builder builder = new Picasso.Builder(this);
         builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
         Picasso built = builder.build();
-        built.setIndicatorsEnabled(true);
+        //built.setIndicatorsEnabled(true);
         //built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
     }
